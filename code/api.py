5477de7ec26de8a846cfd24520f98748c0c2e33c
Jonathan Vicente <jonathanvicenteportugal@gmail.com>
2019-02-27 09:37:35 +0100

    Changement du retour pour rendre le monde meilleur

--- code/api.py
+++ code/api.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! It's ME"
+    return "Hello, World! It's ME!!"
 
 if __name__ == '__main__':
     app.run(debug=True)
